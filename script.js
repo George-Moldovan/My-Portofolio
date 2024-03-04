67b9a60c0c2aac025b2c5cdc38c929d64a9b02cb
George-Moldovan <moldovangd@yahoo.com>
2024-03-04 16:03:06 +0200

    contact form validation

--- script.js
+++ script.js
@@ -69,3 +69,57 @@ prevBtn.addEventListener("click", (e) => {
 if (autoSlide) {
   slideInterval = setInterval(nextSlide, intervalTime);
 }
+
+// ============ Contact Form  ============
+const form = document.getElementById("form");
+const fullName = document.getElementById("name");
+const phone = document.getElementById("phone");
+const email = document.getElementById("email");
+const weddingLocation = document.getElementById("wedding-location");
+const errorElement = document.getElementById("error");
+const clientMessage = document.getElementById("message");
+const successElement = document.getElementById("success-message");
+
+form.addEventListener("submit", (e) => {
+  let messages = [];
+  let success = [];
+  //==================Name Validation==============
+  if (fullName.value === "" || fullName.value == null) {
+    messages.push("Name is required!");
+  }
+  //==================Phone Validation==============
+
+  if (phone.value === "" || phone.value == null) {
+    messages.push("Phone number is required!");
+  }
+  //==================Email Validation==============
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email.value)) {
+    messages.push("Please enter a valid email address!");
+  }
+
+  //==================Wedding Location Validation==============
+  if (weddingLocation.value === "" || weddingLocation.value == null) {
+    messages.push("Location is required!");
+  }
+
+  //==================Wedding Location Validation==============
+  if (clientMessage.value === "" || clientMessage == null) {
+    messages.push("Your message is required!");
+  }
+
+  if (messages.length === 0) {
+    // If there are no validation errors, display success message
+    success.push("The Email was sent!");
+  }
+
+  if (success.length > 0) {
+    e.preventDefault();
+    successElement.innerText = success.join(" \n ");
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerText = messages.join(" \n ");
+  }
+});
